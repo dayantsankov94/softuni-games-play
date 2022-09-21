af094c28b5d5b7d1ac5c5008d5b3495a3c70e10c
dayantsankov94 <daqntsankov@gmail.com>
2022-09-21 18:34:44 +0300

    Render latest games conditionaly

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             .then(result => {
                 setGames(result);
             });
-    },[]);
+    }, []);
 
     return (
         <section id="welcome-world">
@@ -20,10 +20,12 @@ const Home = () => {
             <img src="./images/four_slider_img01.png" alt="hero" />
             <div id="home-page">
                 <h1>Latest Games</h1>
-                {/* Display div: with information about every game (if any) */}
-                {games.map(x => <LatestGame key={x._id} game={x}/>)}
-                {/* Display paragraph: If there is no games  */}
-                <p className="no-articles">No games yet</p>
+
+                {games.length > 0
+                    ? games.map(x => <LatestGame key={x._id} game={x} />)
+                    : <p className="no-articles">No games yet</p>
+                }
+
             </div>
         </section>
     );
