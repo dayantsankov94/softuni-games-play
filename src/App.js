252e06657af00ca57a5e79370600b5e4df4a82ea
dayantsankov94 <daqntsankov@gmail.com>
2022-09-22 00:32:32 +0300

    Add comments to game state

--- src/App.js
+++ src/App.js
@@ -14,12 +14,26 @@ import './App.css';
 
 function App() {
 
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState([]);
+
+    const addComment = (gameId, comment) => {
+        setGames(state => {
+            const game = games.find(x => x._id == gameId);
+
+            const comments = game.comments || [];
+            comments.push(comment)
+            return [
+                ...state.filter(x => x._id !== gameId),
+                {...game, comments}
+            ]
+        })
+    }
+
     useEffect(() => {
         gameService.getAll()
             .then(result => {
                 console.log(result);
-                
+
                 setGames(result);
             });
     }, []);
@@ -30,12 +44,12 @@ function App() {
             {/* Main Content */}
             <main id="main-content">
                 <Routes>
-                    <Route path="/" element={<Home games={games}/>} />
+                    <Route path="/" element={<Home games={games} />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/create" element={<CreateGame />} />
-                    <Route path="/catalog" element={<Catalog games={games}/>} />
-                    <Route path="/catalog/:gameId" element={<GameDetails />} />
+                    <Route path="/catalog" element={<Catalog games={games} />} />
+                    <Route path="/catalog/:gameId/*" element={<GameDetails games={games} addComment={addComment}/>} />
                 </Routes>
             </main>
 
